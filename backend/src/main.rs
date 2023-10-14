a7346b7369b82c615922d7966194107b4be28783
Ed Cashin <ed.cashin@acm.org>
2023-10-13 21:45:15 -0400

    Write score updates to database

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -59,6 +59,29 @@ async fn get_scores(pool: Pool) -> std::result::Result<impl Reply, Infallible> {
     Ok(warp::reply::json(&scores))
 }
 
+async fn post_score(pool: Pool, score: Score) -> std::result::Result<impl Reply, Infallible> {
+    println!("received score update: {score:?}");
+    let client = pool
+        .get()
+        .await
+        .context("getting DB client from pool")
+        .unwrap();
+    let sql = "
+        insert into wherego_scores (username, dest_id, score)
+        values
+        ($1, $2, $3)
+        on conflict (username, dest_id)
+        do update set score = excluded.score
+    ";
+    client
+        .execute(sql, &[&score.username, &score.dest_id, &score.score])
+        .await
+        .context("preparing statement")
+        .unwrap();
+
+    Ok(warp::reply::json(&score))
+}
+
 async fn get_destinations(pool: Pool) -> std::result::Result<impl Reply, Infallible> {
     let client = pool
         .get()
@@ -104,7 +127,15 @@ async fn api_routes(pool: Pool) -> warp::filters::BoxedFilter<(impl Reply,)> {
             .and(warp::any().map(move || pool.clone()))
             .and_then(get_scores)
     };
-    get_destinations.or(get_scores).boxed()
+    let post_score = {
+        let pool = pool.clone();
+        warp::post()
+            .and(warp::path!("api" / "scores"))
+            .and(warp::any().map(move || pool.clone()))
+            .and(warp::body::content_length_limit(1024 * 16).and(warp::body::json()))
+            .and_then(post_score)
+    };
+    get_destinations.or(get_scores).or(post_score).boxed()
 }
 
 #[tokio::main]
--- backend/src/scores.sql
+++ backend/src/scores.sql
@@ -1,3 +1,4 @@
 drop table if exists wherego_scores;
 create table if not exists wherego_scores ( username varchar (256) not null, score bigint, dest_id bigint );
 insert into wherego_scores (username, score, dest_id) values ('Ed', 100, 1);
+create unique index if not exists wherego_user_dest_score_idx on wherego_scores (username, dest_id);
